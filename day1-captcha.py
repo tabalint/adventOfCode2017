aabb8d971eaf4221a94d7390ce3442cf56fa35ae
czarstark <trevor.balint@gmail.com>
2017-12-01 13:42:10 +0000

    Initial Solution to Day 1

---
+++ day1-captcha.py
@@ -0,0 +1,20 @@
+# Week 1: WILL BE MOVING THIS TO ITS OWN FILE WHEN I GET HOME
+# Part 1
+sum = 0
+lastChar=myString[len(myString)-1]
+for char in myString:
+  if char == lastChar:
+    sum += int(lastChar)
+  lastChar = char
+
+print sum
+
+# Part 2
+sum2 = 0
+myStringFirstHalf = myString[:len(myString)/2]
+myStringSecondHalf = myString[len(myString)/2:]
+for a in range(len(myStringFirstHalf)):
+  if myStringFirstHalf[a] == myStringSecondHalf[a]:
+    sum2 += 2*int(myStringFirstHalf[a])
+
+print sum2
